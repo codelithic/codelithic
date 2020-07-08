6b1d44b761374e91a29b6c542ca5fb328a1cfa7f
codelithic <roberto.dirienzo@codelithic.com>
2020-07-08 13:02:02 +0200

    init

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ codelithic framework base
 tested cenotos:7
 
 dependents:
-codelithic --> pthread
-codelithic_web --> codelithic uuid libcurl pthread
-codelithic_site --> codelithic pthread fcgi++ fcgi cgicc
+* codelithic --> pthread
+* codelithic_web --> codelithic uuid libcurl pthread
+* codelithic_site --> codelithic pthread fcgi++ fcgi cgicc
---
+++ include/codelithic/mod/imod.hpp
@@ -0,0 +1,58 @@
+
+#ifndef CLCRBAC_IMOD_HPP
+#define CLCRBAC_IMOD_HPP
+
+#include <dlfcn.h>
+#include <stdexcept>
+#include <string>
+
+namespace codelithic::mods {
+
+//TARGET_LINK_LIBRARIES(clcrbac m dl z)#imod.hpp
+class IModInterface {
+  bool valid{false};
+
+protected:
+public:
+  IModInterface() = delete;
+
+  IModInterface(const IModInterface &) = delete;
+
+  explicit IModInterface(std::string path) : path_(std::move(path)) {
+    handle = dlopen(path_.data(), RTLD_LAZY);
+    if (!handle) {
+      handle = nullptr;
+      setValid(false);
+      setLastError(std::string(dlerror()));
+    }
+    setValid(true);
+    resetError();
+  }
+
+  virtual ~IModInterface() {
+    if (handle != nullptr) {
+      dlclose(handle);
+      handle = nullptr;
+      resetError();
+    }
+  }
+
+  bool isValid() const { return valid; }
+  void setValid(bool val) { IModInterface::valid = val; }
+
+  const std::string &getLastError() const { return lastError; }
+  void resetError() { lastError.clear(); }
+  void setLastError(std::string strError) {
+    IModInterface::lastError = std::move(lastError);
+  }
+
+protected:
+  std::string lastError{""};
+  void *handle{nullptr};
+
+private:
+  std::string path_{""};
+};
+} // namespace mods
+
+#endif // CLCRBAC_IMOD_HPP
